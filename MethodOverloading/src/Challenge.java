b745d5fa7eb37c0bf2822fda3835bb7bc8fc34b2
Dino <dm3986647@gmail.com>
2022-03-12 20:37:23 +0100

    /

--- MethodOverloading/src/Challenge.java
+++ MethodOverloading/src/Challenge.java
@@ -3,6 +3,7 @@ public class Challenge {
 
 	public static void main(String[] args) {
 		calcFeetAndInchesToCentimeters(-1,2);
+		calcFeetAndInchesToCentimeters(100);
 	}
  
 	public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
@@ -16,5 +17,13 @@ public class Challenge {
 		System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
 		return centimeters;
 	}
-	
+	public static double calcFeetAndInchesToCentimeters(double inches) {
+		if(inches < 0) {
+			return -1;
+		}
+	double feet = (int) inches /12;
+	double remainingInches = (int) inches % 12;
+	System.out.println(inches + " inches is equal to " + feet + " feet and "+ remainingInches);
+	return calcFeetAndInchesToCentimeters(feet, remainingInches);
+	}
 }
