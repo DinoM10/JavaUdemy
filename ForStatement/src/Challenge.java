6d5418363364b5aa18310aefb98efbc6f04ecc63
Dino <dm3986647@gmail.com>
2022-03-14 09:34:26 +0100

    -

---
+++ ForStatement/src/Challenge.java
@@ -0,0 +1,18 @@
+
+public class Challenge {
+
+	public static void main(String[] args) {
+		int i;
+	
+			for(i=1; i<=1000; i++) {
+		
+			if(i % 3 == 0 && i % 5 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+	}
+
+	
+
+
