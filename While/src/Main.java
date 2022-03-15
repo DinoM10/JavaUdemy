9c3ebd94b8ea65842eb2a139450421fe91bc6028
Dino <dm3986647@gmail.com>
2022-03-15 10:24:07 +0100

    While

---
+++ While/src/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int count = 6;
+//		while(count != 5) {
+//			System.out.println("Count value is " + count);
+//			count++;
+//		}
+//		
+//		count = 1;
+//		while(true) {
+//			if(count == 6) {
+//				break;
+//			}
+//			System.out.println("Count value is " + count);
+//			count++;
+//		}
+		count = 6;
+		do {
+			System.out.println("Count value was " + count);
+			count++;
+			
+			if(count>100) {
+				break;
+			}
+		}while(count != 6);
+		
+	}
+
+}
